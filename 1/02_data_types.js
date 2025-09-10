0dd7220b4a4f4e288d61eda0dc0206fdfb9f4344
Aniket <aniketkuamrsah07@gmail.com>
2025-09-10 19:05:45 +0530

    Added data types file

---
+++ 1/02_data_types.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs, not browser so it will show error bt if ran in console it would give a pop up of '6'
+
+let name="Aniket"
+let age= 20
+let isLoggedIn= false
+let state  // This is undefined as variable has been declared but no value has been assigned
+
+//data types:
+//number => 2 to power 53
+// bigint
+//string
+//boolean
+//null => standalone value
+//undefined 
+//symbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
