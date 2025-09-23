ac2838606f834a2e69482e253f89e9d7711b57b0
Aniket Kumar Sah <aniketkumarsah07@gmail.com>
2025-09-23 16:22:11 +0000

    Added 04 file

---
+++ 1/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2!=1);
+
+// console.log(null ==0); //null does not get converted to a number in equality checks with 0
+//                        //The == operator (loose equality) only considers null equal to undefined
+// console.log(null>0); //False
+// console.log(null >=0); // true //during comaprison operator null gets converted to 0
+// **One should avoid these kind of comparisons
+// console.log("2"===2);//here === is strict check here no conversion is done.
+
+
