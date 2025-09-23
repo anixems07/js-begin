4ea7abaac627883e74b2c4f6c3507651d938f593
Aniket Kumar Sah <aniketkumarsah07@gmail.com>
2025-09-23 16:00:03 +0000

    added operations

---
+++ 1/03_conversion-operation.js
@@ -0,0 +1,42 @@
+let score="33abc"
+
+// console.log(typeof (score)); // o/p string
+
+let valNumber=Number(score); //here the datatype conversion is done
+// console.log(typeof (valNumber)); // o/p number
+// console.log(valNumber); //this is not a number so it will give NaN when score = 33abc
+                        //if score = 33 then it will give 33 as output
+
+  // "33abc" => NaN
+  // null => 0
+  // true => 1
+
+  let isLoggedIn= ""
+//   console.log(Boolean(isLoggedIn)); false
+
+  // "" => false
+  // "Aniket" => true
+  // 0 => false
+  // 33 => true 
+
+//   ****************************Operations*****************************************
+
+let value=3
+let negVal= -value;
+
+// console.log(negVal); //-3
+
+let str1="hello"
+let str2=" Aniket"
+
+// console.log(str1+str2); //hello Aniket
+
+// console.log(2**3) //2 to the power 3
+
+// console.log(1+2+"2"); //32
+// console.log("1"+2+2); //122
+
+// let x=4
+// x++ //here operation is being done the value will increase to 5
+
+console.log(x);//5
