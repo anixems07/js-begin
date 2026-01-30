c960d833b35422d3e300ea5c0397c94c73123855
Aniket <aniketkuamrsah07@gmail.com>
2026-01-30 19:15:53 +0530

    While and Do while Loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+//while loop
+
+let index=0
+while (index<=10) {
+    // console.log(`value of index is ${index}`);
+    index+=2;
+}
+
+let myArray = [100, 200, 300, 400, 500];
+let arr=0
+
+while (arr<myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+// do while loop
+
+let score=11;
+
+do {
+    console.log(score); //11
+    score=score+1
+    
+} while (score<=5);
